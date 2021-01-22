060f9bf7c9fcc9a9c929385d57c8c6356a8a5630
Harshguglani <harshguglani95@gmail.com>
2021-01-22 13:23:44 +0530

    updated

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://sharad:Vermasaloni@12644@myfirstcluster.q3xza.mongodb.net/DB?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
